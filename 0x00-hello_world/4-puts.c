9c7273a0d9aeb9bc5bca3660a630eb81f64635fd
Nahom Debebe <nahomdebebe0@gmail.com>
2022-03-12 10:53:02 +0300

    message

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+* main - print string from main function
+*
+* Description: using the main function
+* this program prints "programming is like building multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+}

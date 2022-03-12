97bbbc4315f7c0bf28b6765ba706c5672f5d06fb
Nahom Debebe <nahomdebebe0@gmail.com>
2022-03-12 16:52:59 +0300

    message

---
+++ 0x01-variables_if_else_while/6-printt_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main -the main function uses while loop function
+*
+* Description: print base ten single digit numbers
+*
+* Return:0
+*/
+int main(void)
+{
+
+	int n = 48;
+
+	while (n <= 57)
+	{
+	putchar(n);
+	n++;
+	}
+	putchar('\n');
+	return (0);
+}

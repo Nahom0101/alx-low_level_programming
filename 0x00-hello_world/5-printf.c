71379b0bd0283f2beb1e55d40330d1238e9fdb79
Nahom Debebe <nahomdebebe0@gmail.com>
2022-03-12 11:20:34 +0300

    message

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print strings using printf function
+ *
+ * description:using the main function
+ * the program prints with proper grammer, but the outcome is a piece of art,\n
+ * Return: 0
+ */
+
+ int main(void)
+{
+	printf("with proper grammer,but the outcome is a piece of art,\n);
+	return (0);
+}
